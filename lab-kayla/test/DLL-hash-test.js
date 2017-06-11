cf9d46e395649fa829601ce2e2af3d427f90aac6
Kayla Asay <kayla-asay@Ubuntu16.04-01>
2017-06-11 16:34:24 -0700

    testing

--- lab-kayla/test/DLL-hash-test.js
+++ lab-kayla/test/DLL-hash-test.js
@@ -1,9 +1,23 @@
 'use strict'
 
 const expect = require('chai').expect
+const DLL = require('../lib/hash-table')
 
-describe('append method', function() {
-  it('should append a node', done => {
-    
+describe('DDL Hash Table', function() {
+  beforeEach(done => {
+    this.dLL = new DLL()
+    done()
+  })
+  afterEach(done => {
+    this.dLL = null
+    done()
+  })
+})
+describe('append method',() => {
+  it('should append a new node', done => {
+    let newNode = this.dLL('new')
+    this.dLL.set('old', 'new')
+    expect(this.dLL(newNode)).to.equal('new')
+    done()
   })
 })
